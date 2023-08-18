ecee6e2467510764b1100176477741644cefc76a
aryn20022003 <aryan@LAPTOP-8SBBLOK0>
2023-08-18 15:02:08 +0530

    final version of api ready

--- middleware/getCheck.py
+++ middleware/getCheck.py
@@ -3,16 +3,19 @@ from rest_framework.response import Response
 from rest_framework import status
 import json
 
-# not mounted yet 
+
+# not mounted yet
 class sanityCheck:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request) -> Any:
-        if request.method not in ["GET"]:
-            return Response(
-                data={"error": "method not allowed", "status": 405},
+    def __call__(self, request):
+        if request.method != "GET":
+            response = Response(
+                data={"error": "Method not allowed", "status": 405},
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
+            return response
 
-        return self.get_response(request)
+        response = self.get_response(request)
+        return response
